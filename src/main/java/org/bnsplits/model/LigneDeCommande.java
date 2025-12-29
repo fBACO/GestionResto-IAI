2f7ce7579fefa4770a79c22ad3313605318c797d
JeJe <Jewelbesse@gmail.com>
2025-12-29 19:12:58 +0000

    Creation du modele ligne de commande

--- src/main/java/org/bnsplits/model/LigneDeCommande.java
+++ src/main/java/org/bnsplits/model/LigneDeCommande.java
@@ -1,8 +1,7 @@
-
 package org.bnsplits.model;
 
-
 public class LigneDeCommande {
+
     private int id_LigneCmde;
     private int id_cmde;
     private int id_produit;
@@ -10,41 +9,52 @@ public class LigneDeCommande {
     private double prix_unitaire;
     private double montant_ligne;
     
-    public int getId_LigneCmde(){
-        
+//    public void ligneDeCommande(int id_LigneCmde, int id_cmde, int id_produit, in id) {
+//    }
+
+    public void LineDeCommande (int id_LigneCmde, int id_cmde, int id_produit, int quantite, double prix_unitaire, double montant_ligne)
+    {
+    
+     this.id_LigneCmde = id_LigneCmde;
+     this.id_cmde = id_cmde;
+     this.id_produit = id_produit;
+     this.quantite = quantite ;
+     this.prix_unitaire = prix_unitaire;
+     this.montant_ligne = montant_ligne;
+    };
+    
+    public int getId_LigneCmde() {
+
         return id_LigneCmde;
-    
+
     }
-    
-    public int getId_cmde(){
-    
+
+    public int getId_cmde() {
+
         return id_cmde;
     }
-    
-    public int getId_produit (){
-    
+
+    public int getId_produit() {
+
         return id_produit;
     }
-    
-    public int getQuantite (){
-    
+
+    public int getQuantite() {
+
         return quantite;
-        
+
     }
-    
-    public double getPrix_unit(){
-    
+
+    public double getPrix_unit() {
+
         return prix_unitaire;
-        
+
     }
-    
-    public double getMontant_ligne(){
-    
+
+    public double getMontant_ligne() {
+
         return montant_ligne;
-        
+
     }
-    
-    
-    
-    
+
 }

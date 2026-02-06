1870f20cafc060420766d51de1b50ffa7f244dda
BnSplits <bnsp.x0@gmail.com>
2026-02-06 15:13:36 +0000

    Rajout des props 'username' et 'type' à Utilisateur

--- src/main/java/org/bnsplits/model/entities/Utilisateur.java
+++ src/main/java/org/bnsplits/model/entities/Utilisateur.java
@@ -3,39 +3,50 @@ package org.bnsplits.model.entities;
 import org.bnsplits.util.HashUtil;
 
 public class Utilisateur {
-    private int idUtilisateur;
-    private String login;
-    private String motDePasse;
+  private int idUtilisateur;
+  private String login;
+  private String username;
+  private String motDePasse;
 
-    public Utilisateur(){};
+  public Utilisateur() {
+  };
 
-    public Utilisateur(int idUtilisateur, String login, String motDePasse) {
-        this.idUtilisateur = idUtilisateur;
-        this.login = login;
-        this.motDePasse = HashUtil.sha256Hash(motDePasse);
-    }
+  public Utilisateur(int idUtilisateur, String login, String username, String motDePasse) {
+    this.idUtilisateur = idUtilisateur;
+    this.login = login;
+    this.username = username;
+    this.motDePasse = HashUtil.sha256Hash(motDePasse);
+  }
 
-    public int getIdUtilisateur() {
-        return idUtilisateur;
-    }
+  public int getIdUtilisateur() {
+    return idUtilisateur;
+  }
 
-    public String getLogin() {
-        return login;
-    }
+  public String getLogin() {
+    return login;
+  }
 
-    public String getMotDePasse() {
-        return motDePasse;
-    }
+  public String getUsername() {
+    return username;
+  }
 
-    public void setIdUtilisateur(int idUtilisateur) {
-        this.idUtilisateur = idUtilisateur;
-    }
+  public String getMotDePasse() {
+    return motDePasse;
+  }
 
-    public void setLogin(String login) {
-        this.login = login;
-    }
+  public void setIdUtilisateur(int idUtilisateur) {
+    this.idUtilisateur = idUtilisateur;
+  }
 
-    public void setMotDePasse(String motDePasse) {
-        this.motDePasse = HashUtil.sha256Hash(motDePasse);
-    }
+  public void setLogin(String login) {
+    this.login = login;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public void setMotDePasse(String motDePasse) {
+    this.motDePasse = HashUtil.sha256Hash(motDePasse);
+  }
 }

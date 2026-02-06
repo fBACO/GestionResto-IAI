a6106112ec2026f5ce285639af6f4e37299f6848
BnSplits <bnsp.x0@gmail.com>
2026-02-06 09:20:53 +0000

    Initialisation de la classe CustomButton

---
+++ src/main/java/org/bnsplits/view/components/CustomButton.java
@@ -0,0 +1,10 @@
+package org.bnsplits.view.components;
+
+import com.formdev.flatlaf.FlatClientProperties;
+import javax.swing.*;
+import java.awt.*;
+import org.kordamp.ikonli.fontawesome6.*;
+import org.kordamp.ikonli.swing.FontIcon;
+
+public class CustomButton extends JButton {
+}

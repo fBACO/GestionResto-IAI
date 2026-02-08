632c4f608894581fb313eee815cec51f5b9d2493
BnSplits <bnsp.x0@gmail.com>
2026-02-08 10:36:05 +0000

    Initialisation du fichier Theme

---
+++ src/main/java/org/bnsplits/view/Theme.java
@@ -0,0 +1,22 @@
+package org.bnsplits.view;
+
+public class Theme {
+
+  public static final String PRIMARY_COLOR = "#5A7388";
+  public static final String ON_PRIMARY_COLOR = "#FFFFFF";
+
+  public static final String BACKGROUND_COLOR = "#F6F7FB";
+  public static final String ON_BACKGROUND_COLOR = "#000000";
+
+  public static final String SURFACE_COLOR = "#FFFFFF";
+  public static final String ON_SURFACE_COLOR = "#000000";
+
+  public static final String SURFACE_DIM_COLOR = "#E5E7EB";
+  public static final String ON_SURFACE_DIM_COLOR = "#6B7280";
+
+  public static final String OUTLINE_COLOR = "#EFF0F3";
+
+  public static final String ERROR_COLOR = "#DC2626";
+  public static final String WARNING_COLOR = "#F59E0B";
+  public static final String SUCCESS_COLOR = "#22C55E";
+}
